3e7d795072342cf146dde454df8149e96e0e3e40
happyyi(易罗阳) <buutuud@gmail.com>
2017-10-21 21:15:58 +0800

    构建代码添加

--- cmd/lightsocks-local/build.sh
+++ cmd/lightsocks-local/build.sh
@@ -1,13 +1,13 @@
 # @Author: happyyi
 # @Date:   2017-10-20 21:54:37
-# @Last Modified by:   happyyi
-# @Last Modified time: 2017-10-20 21:55:40
+# @Last Modified by:   happyyi(易罗阳)
+# @Last Modified time: 2017-10-20 23:27:22
 # GOOS=linux GOARCH=amd64 go build -o lsocks .
-GOOS=linux GOARCH=386 go build -o lsocks_linux_386 .
+# GOOS=linux GOARCH=386 go build -o lsocks_linux_386 .
 # GOOS=linux GOARCH=arm64 go build -o lsocks_linux_arm64 .
 # GOOS=linux GOARCH=arm GOARM=7 go build -o lsocks_linux_arm7 .
 # GOOS=linux GOARCH=arm GOARM=6 go build -o lsocks_linux_arm6 .
 # GOOS=linux GOARCH=arm GOARM=5 go build -o lsocks_linux_arm5 .
 GOOS=darwin GOARCH=amd64 go build -o lsocks_macos_amd64 .
-GOOS=windows GOARCH=amd64 go build -o lsocks_windows_amd64.exe .
+# GOOS=windows GOARCH=amd64 go build -o lsocks_windows_amd64.exe .
 # GOOS=windows GOARCH=386 go build -o lsocks_windows_386.exe .
