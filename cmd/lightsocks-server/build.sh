a93d88f0d61915e97fa4136760f329d706986ef7
happyyi(易罗阳) <buutuud@gmail.com>
2017-11-11 13:58:02 +0800

    add bin

--- cmd/lightsocks-server/build.sh
+++ cmd/lightsocks-server/build.sh
@@ -1,7 +1,7 @@
 # @Author: happyyi
 # @Date:   2017-10-20 21:54:37
 # @Last Modified by:   happyyi(易罗阳)
-# @Last Modified time: 2017-10-20 23:27:22
+# @Last Modified time: 2017-11-11 13:55:20
 # GOOS=linux GOARCH=amd64 go build -o lsocks .
 GOOS=linux GOARCH=386 go build -o lsocks_linux_386 .
 # GOOS=linux GOARCH=arm64 go build -o lsocks_linux_arm64 .
@@ -11,3 +11,6 @@ GOOS=linux GOARCH=386 go build -o lsocks_linux_386 .
  GOOS=darwin GOARCH=amd64 go build -o lsocks_macos_amd64 .
 # GOOS=windows GOARCH=amd64 go build -o lsocks_windows_amd64.exe .
 # GOOS=windows GOARCH=386 go build -o lsocks_windows_386.exe .
+mkdir ../../bin/lightsocks-server/
+mv lsocks_linux_386 ../../bin/lightsocks-server/
+mv lsocks_macos_amd64 ../../bin/lightsocks-server/
